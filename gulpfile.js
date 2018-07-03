e08a82cc1e0df25ae22a80ce7befd2773643e3e4
Lyudmila <laudylim@yandex.ru>
2018-07-03 16:05:17 +0300

    htmlbeautify added

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var del = require('del');
 var runSequence = require('run-sequence');
 var watch = require('gulp-watch');
+var htmlbeautify = require('gulp-html-beautify');
 
 // SVG sprites
 var svgmin = require('gulp-svgmin');
@@ -96,12 +97,34 @@ gulp.task('pug', function() {
 			}
 		})
 	}))
-	.pipe(pug({
-		pretty: true
-	}))
+	.pipe(pug())
+	.pipe(htmlbeautify(htmlbeautifyOptions))
 	.pipe(gulp.dest('./build'))
 	.pipe(browserSync.stream());
 });
+
+var htmlbeautifyOptions = {
+	"indent_size": 1,
+	"indent_char": "	",
+	"eol": "\n",
+	"indent_level": 0,
+	"indent_with_tabs": true,
+	"preserve_newlines": false,
+	"max_preserve_newlines": 10,
+	"jslint_happy": false,
+	"space_after_anon_function": false,
+	"brace_style": "collapse",
+	"keep_array_indentation": false,
+	"keep_function_indentation": false,
+	"space_before_conditional": true,
+	"break_chained_methods": false,
+	"eval_code": false,
+	"unescape_strings": false,
+	"wrap_line_length": 0,
+	"wrap_attributes": "auto",
+	"wrap_attributes_indent_size": 4,
+	"end_with_newline": false
+};
 
 gulp.task('svg', function() {
 	return gulp.src('./src/img/svg-for-sprites/*.svg')
