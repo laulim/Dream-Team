8b6f724e1ec90de506f63eb89ac94552a2ac43cd
Lyudmila <laudylim@yandex.ru>
2018-07-04 02:52:04 +0300

    login-form-check script ready

--- src/js/main.js
+++ src/js/main.js
@@ -2,16 +2,5 @@ $(document).ready(function() {
 	
 	$(".left-panel").customScrollbar({preventDefaultScroll: true});
 
-	
-	var inputEmail = $('#email').val();
-	var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
-	var formLogin = $('.login-page__form');
-
-	formLogin.on('submit', function(event){
-		event.preventDefault();
-		if( inputEmail == "" || inputEmail !== pattern ){
-			$('.error__title').fadeIn();
-		}
-	});
 
 });
